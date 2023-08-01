e8d86c59e4187a79cf6b4df6c364686ed83ad31b
wlangera <ward.langeraert@inbo.be>
2023-08-01 17:29:00 +0200

    clean up code to 1

---
+++ source/set_priors_brms.R
@@ -0,0 +1,41 @@
+set_priors_brms <- function(combo, df, formula) {
+  require("dplyr")
+
+  trait1 <- str_split_i(combo, "_", 1)
+  trait2 <- str_split_i(combo, "_", 2)
+
+  present_combinations <- df %>%
+    mutate(group = paste(paste0(trait1, !!sym(trait1)),
+                         paste0(trait2, !!sym(trait2)), sep = ":")) %>%
+    distinct(group) %>%
+    pull()
+
+  all_combinations <- df %>%
+    expand(!!sym(trait1), !!sym(trait2)) %>%
+    mutate(group = paste(paste0(trait1, !!sym(trait1)),
+                         paste0(trait2, !!sym(trait2)), sep = ":")) %>%
+    pull(group)
+
+
+
+  missing_combinations <- gsub("(\\s|\\(|\\))", "",
+                               setdiff(all_combinations, present_combinations))
+
+
+  interactions <- paste("periodp2013_2022", missing_combinations, sep = ":")
+
+  priors <- get_prior(
+    formula = formula,
+    data = df,
+    family = poisson())
+
+  if (length(missing_combinations) == 0) {
+    prior_out <- priors
+  } else {
+    prior_out <- priors %>%
+      mutate(prior = ifelse(coef %in% c(missing_combinations, interactions),
+                            "constant(0)", prior))
+  }
+
+  return(prior_out)
+}
